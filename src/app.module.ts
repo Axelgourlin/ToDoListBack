e135602a76aeddf70b66dd4d2241ad73cb4da2f2
Axel Gourlin <gourlin.axel@gmail.com>
2021-12-31 14:49:23 +0100

    stay

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      debug: true,
+      playground: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '903029',
+      database: 'Todo',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
